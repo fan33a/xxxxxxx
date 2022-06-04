e69bd791a289002364f5dc97be6e868a3181a80c
fan33a <42098159+fan33a@users.noreply.github.com>
2022-06-05 00:28:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,7 @@ async def on_connect():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startwith('hi'):
-        await message.channel.send('Hello!'):
+    await message.channel.send('Hello!')
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
