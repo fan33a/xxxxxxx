f43e00b999eadece79a8d96aaf9f580ff9c3af83
fan33a <42098159+fan33a@users.noreply.github.com>
2022-06-05 00:32:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,6 @@ client = discord.Client()
 async def on_connect():
   await client.change_presence(activity = discord.Game(name = "Hi!"))
 
-
-@clientevent
-async def on_message(message):
-    if message.author == client.user:
-        return
-    await message.channel.send('Hello!')
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
