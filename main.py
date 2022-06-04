f9228368f3fb7edba151419f34f84eea72545d74
fan33a <42098159+fan33a@users.noreply.github.com>
2022-06-05 00:16:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+@clientevent
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startwith('hi'):
+        await message.channel.send('Hello!'):
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
