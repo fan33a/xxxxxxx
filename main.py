952e05159e78f847e659ba0cd019f82505462ff7
fan33a <42098159+fan33a@users.noreply.github.com>
2022-06-05 02:03:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
+import discord, os, keep_alive, asyncio, datetime, pytz
 
-import discord
+
+from discord.ext import tasks, commands
+
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('Fan3a')
 
 client = discord.Client()
 
@@ -12,9 +14,20 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+
+client = commands.Bot(
+  command_prefix='409034645343174657',
+  self_bot=True
+)
+
+
+
 @client.event
 async def on_connect():
   await client.change_presence(activity = discord.Streaming(name = "ملل😴", url = "https://www.twitch.tv/Fan3a"))
 
 
-client.run(Fan3a)
+keep_alive.keep_alive()
+client.run(os.getenv("Fan3a"), bot=False)
+
+
